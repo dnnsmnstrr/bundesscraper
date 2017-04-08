23959cadc1af224c76e6f1425c542a8af0d7e01d
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2017-04-08 16:00:41 +0200

    [Visualisation] Removed unused assets, components, pages…

--- visual/pages/index.vue
+++ visual/pages/index.vue
@@ -1,16 +1,6 @@
 <template>
   <section class="container">
-    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
-    <h1 class="title">
-      USERS
-    </h1>
-    <ul class="users">
-      <li v-for="(user, index) in users" class="user">
-        <nuxt-link :to="{ name: 'id', params: { id: index }}">
-          {{ user.name }}
-        </nuxt-link>
-      </li>
-    </ul>
+
   </section>
 </template>
 
@@ -18,17 +8,6 @@
 import axios from '~plugins/axios'
 
 export default {
-  async asyncData () {
-    let { data } = await axios.get('/api/users')
-    return {
-      users: data
-    }
-  },
-  head () {
-    return {
-      title: 'Users'
-    }
-  }
 }
 </script>
 
