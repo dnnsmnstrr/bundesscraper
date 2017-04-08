94c6040e8d86d8ea97b14cafca2a59937a308aa5
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2017-04-08 22:07:02 +0200

    [Visualisation] Move chart data into due component data

--- visual/pages/index.vue
+++ visual/pages/index.vue
@@ -9,39 +9,48 @@ import axios from '~plugins/axios'
 const test = require('../assets/text/test.json')
 
 export default {
+  data() {
+    return {
+      query: "Migration",
+      data: {
+        datasets: [
+          {
+            label: 'Daten',
+            data: [
+              {
+                  x: 20,
+                  y: 30,
+                  r: 15
+              }
+            ],
+            backgroundColor:"#FF6384",
+            hoverBackgroundColor: "#FF6384",
+          }
+        ]
+      }
+    }
+  },
   mounted() {
     var Chart = require('chart.js')
-    var ctx = document.getElementById("myChart");
-    var data = {
-        datasets: [
-            {
-                label: 'First Dataset',
-                data: [
-                    {
-                        x: 20,
-                        y: 30,
-                        r: 15
-                    },
-                    {
-                        x: 40,
-                        y: 10,
-                        r: 10
-                    }
-                ],
-                backgroundColor:"#FF6384",
-                hoverBackgroundColor: "#FF6384",
-            }]
-    };
-
+    var ctx = document.getElementById("myChart")
     var myBubbleChart = new Chart(ctx,{
       type: 'bubble',
-      data: data
-    });
+      data: this.data
+    })
   }
 }
 </script>
 
 <style scoped>
+input {
+  padding: 0.25em 0.5em;
+  margin-bottom: 1em;
+  border-radius: 2px;
+  border-style: solid;
+  border-width: 1px;
+  border-color: #d4d0d0;
+  font-size: 1.2em;
+}
 .canv {
   width: 70vw;
   height: 60vh;
