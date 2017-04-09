3019aa96b23e1f9a51e0e5682f8f3bb15d7e7669
Arne Schlüter <arne@schlueter.is>
2017-04-09 10:20:38 +0200

    Add jupyter notebooks

--- pre/wordC.py
+++ pre/wordC.py
@@ -1,20 +1,20 @@
 import re
+import sys
 from collections import Counter
 
-with open('file.txt') as f:
+with open(sys.argv[1]) as f:
     passage = f.read()
 
 words = re.findall(r'\w+', passage)
-#stopwords = ['UND', 'DIE', 'IN', 'SICH', 'HAT', 'DURCH', 'DER']
 
 with open('stopwords.txt') as stoppi:
     stopwords = stoppi.readlines()
 stopwords = [x.strip().upper() for x in stopwords]
-        
-cap_words = [word.upper() for word in words if word.upper() not in stopwords]
 
+cap_words = [word.upper() for word in words if word.upper() not in stopwords]
 
 word_counts = Counter(cap_words)
 
-
-print(word_counts.most_common(10))
+print(sys.argv[1])
+print(word_counts.most_common(25))
+print()
